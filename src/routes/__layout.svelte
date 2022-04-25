8fe85ea8e9ca9205353149ca0a0b36a90cef57fe
chanwit-y <chanwit.yimneam@hotmail.com>
2022-04-25 09:57:01 +0700

    test tick

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import AppBar from '$lib/layouts/app-bar.svelte';
+
+	import '../app.css';
+</script>
+
+<div class="dark">
+	<AppBar />
+	<div class="bg-gray-100 h-screen">
+		<slot />
+	</div>
+</div>
